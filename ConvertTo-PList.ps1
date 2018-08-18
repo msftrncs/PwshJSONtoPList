e5c3bb40a43aef38b4e2de998928abbaf9d3c7eb
msftrncs <msftrncs@midlands.net>
2018-08-18 02:53:33 -0500

    Add files via upload

--- ConvertTo-PList.ps1
+++ ConvertTo-PList.ps1
@@ -1,6 +1,8 @@
 # attempt to convert a JSON textmate language file back to PLIST tmLanguage file
 
-function make-plist ($name, $item, [bool]$isArray, $indent) {
+function make-plist ($name, $item, [bool]$isArray, [string]$indent) {
+    # recursively break down the objects based on their type
+    # single item array types get converted to non array types when passed, so this information has to be tracked via $isArray
     "$indent<key>$([System.Web.HttpUtility]::HtmlEncode($name))</key>"
     if ($isArray) {
         # handle most arrays or items that should have been arrays
@@ -11,26 +13,20 @@ function make-plist ($name, $item, [bool]$isArray, $indent) {
                 "$indent`t<string>$([System.Web.HttpUtility]::HtmlEncode($subitem))</string>"
             }
         }
-        elseif ($item -is [System.Object[]]) {
+        else {
             # handle an array of objects
             foreach ($subitem in $item) {
                 "$indent`t<dict>"
-                $subitem | get-member -type noteproperty | ForEach-Object { make-plist $_.name $(Invoke-Expression "`$subitem.$($_.name)") $(Invoke-Expression "`$subitem.$($_.name) -is [array]") "$indent`t`t" }
+                $subitem | Get-Member -type noteproperty | ForEach-Object { make-plist $_.Name $(Invoke-Expression "`$subitem.$($_.Name)") $(Invoke-Expression "`$subitem.$($_.Name) -is [array]") "$indent`t`t" }
                 "$indent`t</dict>"
             }
-        }
-        else {
-            # handle the 'patterns' that come in as a single item array
-            "$indent`t<dict>"
-            $item | get-member -type noteproperty | ForEach-Object { make-plist $_.name $(Invoke-Expression "`$item.$($_.name)") $(Invoke-Expression "`$item.$($_.name) -is [array]") "$indent`t`t" }
-            "$indent`t</dict>"
         }
         "$indent</array>"
     }
     elseif ($item -is [System.Management.Automation.PSCustomObject]) {
         # handle non array objects
         "$indent<dict>"
-        $item | get-member -type noteproperty | ForEach-Object { make-plist $_.name $(Invoke-Expression "`$item.$($_.name)") $(Invoke-Expression "`$item.$($_.name) -is [array]") "$indent`t" }
+        $item | Get-Member -type noteproperty | ForEach-Object { make-plist $_.Name $(Invoke-Expression "`$item.$($_.Name)") $(Invoke-Expression "`$item.$($_.Name) -is [array]") "$indent`t" }
         "$indent</dict>"
     }
     elseif ($item -is [string] ) {
@@ -38,7 +34,7 @@ function make-plist ($name, $item, [bool]$isArray, $indent) {
         "$indent<string>$([System.Web.HttpUtility]::HtmlEncode($item))</string>"
     }
     else {
-        throw "unknown type $($item.gettype().name)"
+        throw "unhandled type $($item.gettype().Name)"
     }
 }
 
@@ -48,21 +44,19 @@ $FirstLevelObjects = @(
     'repository'
 )
 
+# start by reading in the file through ConvertFrom-JSON
+$grammer_json = Get-Content powershell.tmlanguage.json | ConvertFrom-Json
 
-
-$grammer_json = get-content powershell.tmlanguage.json | convertfrom-json
-
-#$grammer_json | get-member -type noteproperty | ForEach-Object {Write-Output "<key>$($_.name)</key><string>$($grammer_json | select-object -expandproperty $_.name)</string>"}
-
+# write the PLIST header out
 '<?xml version="1.0" encoding="UTF-8"?>'
 '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">'
 '<plist version="1.0">'
-"<dict>"
+"<dict>" #technically this is the begining of the first level
 
+# write out a fixed 'fileTypes' property.
 make-plist "fileTypes" ([string[]]$('ps1', 'psm1', 'psd1')) $true "`t"
 
-# only pass the first items if they match 'name', 'patterns', or 'repository'
-#$grammer_json | get-member -type noteproperty | Where-Object Name -in $FirstLevelObjects | ForEach-Object { make-plist $_.name $(if ($_.name -eq 'patterns') {@($grammer_json | select-object -expandproperty $_.name)} ELSE {$grammer_json | select-object -expandproperty $_.name}) "`t" }
+# only pass the first items if they match 'name', 'patterns', or 'repository', the items will recurse.
+$grammer_json | Get-Member -type noteproperty | Where-Object Name -in $FirstLevelObjects | ForEach-Object { make-plist $_.Name $(Invoke-Expression "`$grammer_json.$($_.Name)") $(Invoke-Expression "`$grammer_json.$($_.Name) -is [array]") "`t" }
 
-$grammer_json | get-member -type noteproperty | Where-Object Name -in $FirstLevelObjects | ForEach-Object { make-plist $_.name $(Invoke-Expression "`$grammer_json.$($_.name)") $(Invoke-Expression "`$grammer_json.$($_.name) -is [array]") "`t" }
-
+# the PList file has now been written to stdout
