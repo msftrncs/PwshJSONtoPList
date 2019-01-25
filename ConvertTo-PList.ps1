995bbbf591bd6b9c327a158c1c50ff89ebe3910d
Carl Morris <msftrncs@midlands.net>
2019-01-25 16:30:18 -0600

    progress towards #2

--- ConvertTo-PList.ps1
+++ ConvertTo-PList.ps1
@@ -1,31 +1,32 @@
 # attempt to convert a JSON textmate language file back to PLIST tmLanguage file
 
 # parameters to the script - needs work to support multiple paths  (WIP)
-[CmdletBinding()]
-Param(
-
-    # Specifies a path to one or more locations. Wildcards are permitted.
-    [Parameter(Mandatory = $true,
-        Position = 0,
-        ValueFromPipeline = $true,
-        ValueFromPipelineByPropertyName = $true,
-        HelpMessage = "Path to one or more locations.")]
-    [ValidateNotNullOrEmpty()]
-    [SupportsWildcards()]
-    [string[]]$Path,
-
-    [Parameter(Mandatory = $false,
-        Position = 1,
-        ValueFromPipeline = $false,
-        ValueFromPipelineByPropertyName = $false,
-        HelpMessage = "Indention pattern.")]
-    [ValidateNotNull()]
-    [string]$Indent = "`t"
-
-)
+#[CmdletBinding()]
+#Param(
+#
+#    [Parameter(Mandatory = $false,
+#        Position = 1,
+#        ValueFromPipeline = $false,
+#        ValueFromPipelineByPropertyName = $false,
+#        HelpMessage = "Indention pattern.")]
+#    [ValidateNotNull()]
+#    [string]$Indent = "`t"
+#
+#)
 
 # define a function to create a plist document, trying to keep it as generic as possible
-function ConvertTo-PList ($PropertyList, [string]$indent) {
+function ConvertTo-PList 
+(
+    [Parameter(Mandatory = $true,
+        ValueFromPipeline = $true,
+        ValueFromPipelineByPropertyName = $true)]
+    $PropertyList, 
+
+    [ValidateNotNull()]
+    [string]$Indent = "`t",
+
+    [switch]$IndentFirstItem
+) {
     # write out a PList document based on the property list supplied
     # $PropertyList is an object containing the entire property list tree.  Hash tables are supported.
     # $indent is a string representing the indentation to use.
@@ -82,7 +83,7 @@ function ConvertTo-PList ($PropertyList, [string]$indent) {
                 "$level<dict>"
                 # iterate through the items (force to a PSCustomObject for consistency)
                 foreach ($property in ([PSCustomObject]$item).psobject.Properties) {
-                    writeproperty $property.Name $property.Value "$level$indent"
+                    writeproperty $property.Name $property.Value "$level$Indent"
                 }
                 "$level</dict>"
             }
@@ -98,14 +99,14 @@ function ConvertTo-PList ($PropertyList, [string]$indent) {
                 # handle an array of bytes, encode as BASE64 string, write as DATA block
                 # use REGEX to split out the string in to 44 character chunks properly indented
                 "$level<data>"
-                [regex]::Matches([convert]::ToBase64String($item), '(.{1,44})').value.foreach( {"$level$indent$_"} )
+                [regex]::Matches([convert]::ToBase64String($item), '(.{1,44})').value.foreach( {"$level$Indent$_"} )
                 "$level</data>"
             }
             else {
                 "$level<array>"
                 # iterate through the items in the array
                 foreach ($subitem in $item) {
-                    writevalue $subitem "$level$indent"
+                    writevalue $subitem "$level$Indent"
                 }
                 "$level</array>"
             }
@@ -122,34 +123,8 @@ function ConvertTo-PList ($PropertyList, [string]$indent) {
     '<plist version="1.0">'
 
     # start writing the property list, the property list should be an object, has no name, and starts at base level
-    writeproperty $null $PropertyList ""
+    writeproperty $null $PropertyList $(if ($IndentFirstItem.IsPresent) {$Indent} else {""} )
 
     # end the PList document
     '</plist>'
-}
-
-<# this lists out the first level of properties to create the PList document from, and also gives the output order of the first level.
-$FirstLevelObjects = @(
-    'name'
-    'patterns'
-    'repository'
-    'scopeName'
-) #>
-
-# from here on, we're converting the PowerShell.tmLanguage.JSON file to PLIST with hardcoded conversion requirements
-foreach ($file in $Path) {
-
-    # start by reading in the file through ConvertFrom-JSON
-    $grammer_json = Get-Content $file | ConvertFrom-Json
-
-    # write the PList document from a custom made object, supplying some data missing from the JSON file, ignoring some JSON objects
-    # and reordering the items that remain.
-    ConvertTo-Plist ([ordered]@{
-            fileTypes                                            = @('ps1', 'psm1', 'psd1')
-            $grammer_json.psobject.Properties['name'].Name       = $grammer_json.psobject.Properties['name'].Value
-            $grammer_json.psobject.Properties['patterns'].Name   = $grammer_json.psobject.Properties['patterns'].Value
-            $grammer_json.psobject.Properties['repository'].Name = $grammer_json.psobject.Properties['repository'].Value
-            $grammer_json.psobject.Properties['scopeName'].Name  = $grammer_json.psobject.Properties['scopeName'].Value
-            uuid                                                 = 'f8f5ffb0-503e-11df-9879-0800200c9a66'
-        }) $Indent
 }
